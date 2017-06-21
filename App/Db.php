e1f0effd8cc6cef517ec2b96c4b06dd6e8a62bc0
Kovtun Alexandr <bocr.step@gmail.com>
2017-06-21 21:58:05 +0400

    + добавлена автозагрузка

---
+++ App/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 21.06.2017
+ * Time: 20:46
+ */
+
+namespace App;
+
+
+class Db
+{
+    public function  __construct()
+    {
+        echo 'Hello Db!';
+    }
+}
